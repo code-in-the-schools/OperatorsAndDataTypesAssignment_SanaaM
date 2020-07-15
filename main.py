b427895112f18c42ac1d6958d333fe9c34f4c7ed
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-15 17:17:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+annual_rainfall_2019 = "annual_rainfall_2019"
+
+january_rainfall_2019 = 3.15
+february_rainfall_2019 = 3.64
+march_rainfall_2019 = 4.14
+april_rainfall_2019 = 3.25
+may_rainfall_2019 = 4.21 
+june_rainfall_2019 = 3.23
+july_rainfall_2019 = 4.23 
+august_rainfall_2019 = 4.85
+september_rainfall_2019 = 2.3 
+october_rainfall_2019 = 2.34
+november_rainfall_2019 = 1.23 
+december_rainfall_2019 = 4.29
+
+winter_rainfall_2019 = january_rainfall_2019 + february_rainfall_2019 + march_rainfall_2019 + april_rainfall_2019 + may_rainfall_2019 + june_rainfall_2019 + july_rainfall_2019 + august_rainfall_2019 + september_rainfall_2019 + october_rainfall_2019 + november_rainfall_2019 + december_rainfall_2019 
+avg_winter_rainfall_2019 = winter_rainfall_2019 / 12
+
+print(" The total winter rainfall is", winter_rainfall_2019,"inches.")
+print( "The average winter rainfall is", avg_winter_rainfall_2019, "inches. ")
+
+annual_rainfall_2019 = 0 
+annual_rainfall_2019 += winter_rainfall_2019
+
+a =  january_rainfall_2019
+b = february_rainfall_2019
+c = march_rainfall_2019
+d = april_rainfall_2019
+e = may_rainfall_2019
+f = june_rainfall_2019
+g = july_rainfall_2019
+h = august_rainfall_2019
+i = september_rainfall_2019
+j = october_rainfall_2019 
+k = november_rainfall_2019
+l = december_rainfall_2019 
+
+avg = (a+b+c+d+e+f+g+h+i+j+k+l)/12
+print (avg) 
+
+(a) = "spring"
+(b) = "summer" 
+(c) = "fall" 
+
+print("The total summer rainfall is",summer_rainfall_2019, "inches.")
